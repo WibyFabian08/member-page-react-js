9d8b401f84eb78bee8c87a0d36977c15d7bee3dc
WibyFabian08 <wibyfabian08@gmail.com>
2021-04-29 14:42:59 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/style.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mt-5">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
